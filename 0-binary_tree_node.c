4649f2056d848947722417a940a9070f63062836
Kelvin Rugut <krugut36@gmail.com>
2024-01-29 14:23:03 +0300

    Creating a New Node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,8 +2,8 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * *parent: poiner to parent node
- * value: value of node
+ * @parent: poiner to parent node
+ * @value: value of node
  *
  * Return: returns a new node address
  */
@@ -14,11 +14,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	newnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
 
 	if (!newnode)
-		return NULL;
+		return (NULL);
 	newnode->n = value;
 	newnode->parent = parent;
 	newnode->left = NULL;
 	newnode->right = NULL;
 
-	return newnode;
+	return (newnode);
 }
