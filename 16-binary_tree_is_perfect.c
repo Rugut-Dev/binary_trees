fbed6e059dc62ff010b1102636ec19c2156393bd
Kelvin Rugut <krugut36@gmail.com>
2024-01-30 12:42:30 +0300

    Checks if perfect binary

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -9,6 +9,7 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t height_l, height_r;
+
 	if (!tree)
 		return (0);
 
@@ -45,6 +46,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	size_t height, size;
+
 	if (!tree)
 		return (0);
 
