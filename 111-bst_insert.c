a924c6bbfea6c946371c340ca40d0d12b761efbe
Kelvin Rugut <krugut36@gmail.com>
2024-01-31 11:38:08 +0300

    insertion in a BST

---
+++ 111-bst_insert.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * bst_insert - inserts a value in a BST
+ * @tree: double ptr to the root node
+ * @value: Value to be stored
+ *
+ * Return: ptr
+ */
+bst_t *bst_insert(bst_t **tree, int value)
+{
+	if (!tree)
+		return (NULL);
+
+	if (*tree == NULL)
+	{
+		*tree = binary_tree_node(NULL, value);
+		return (*tree);
+	}
+
+	if (value < (*tree)->n)
+	{
+		if ((*tree)->left == NULL)
+		{
+			(*tree)->left = binary_tree_node(*tree, value);
+			return ((*tree)->left);
+		}
+		else
+			return (bst_insert(&((*tree)->left), value));
+	}
+	else if (value > (*tree)->n)
+	{
+		if ((*tree)->right == NULL)
+		{
+			(*tree)->right = binary_tree_node(*tree, value);
+			return ((*tree)->right);
+		}
+		else
+			return (bst_insert(&((*tree)->right), value));
+	}
+	return (NULL);
+}
